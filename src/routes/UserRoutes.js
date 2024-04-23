06a814e7d13a2dfb2bf8f3ceca77a992ee6297b0
lebaron567 <emericbarotin@gmail.com>
2024-04-23 16:31:05 +0200

    :white_check_mark: | add route allProfessor

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const user = require("../controllers/UserControllers.js");
 
 router.get("/", user.UserSelect);
+router.get("/professor", user.getAllProfessors);
 router.post("/add", user.addUser);
 
 
